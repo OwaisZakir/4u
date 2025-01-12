270fcaa88fab5e26d0c089f9e0911d0ef318590f
owaiszakir <owaiszakir88@gmail.com>
2025-01-12 22:17:42 +0500

    Update date

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-  const targetDate = new Date("june 1, 2025 00:00:00").getTime();
+  const targetDate = new Date("june 3, 2025 00:00:00").getTime();
   const now = new Date().getTime();
   const timeDifference = targetDate - now;
 
